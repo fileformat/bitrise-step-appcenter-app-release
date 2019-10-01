1869e8c48ab02bcb111007a7d9962aba4ff98e23
Andrew M <fileformat@gmail.com>
2019-10-01 10:57:48 -0400

    Tests

---
+++ test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# test various settings
+#
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+
+#
+# .env must have the following set to valid values:
+# APPCENTER_API_TOKEN
+# APPCENTER_ORG
+# APPCENTER_NAME
+# ARTIFACT_PATH
+#
+export $(cat .env)
+
+echo "INFO: testing with defaults"
+bitrise run test
+
+echo "INFO: testing to 1 group, no notifications"
+export DISTRIBUTION_GROUPS=x-test-group-1
+export NOTIFY_TESTERS=false
+bitrise run test
+
+echo "INFO: testing to second group, w/notifications and release notes"
+export DISTRIBUTION_GROUPS=x-test-group-2
+export NOTIFY_TESTERS=true
+export RELEASE_NOTES="Hey there, just testing at $(date)"
+bitrise run test
+
+echo "INFO: testing to two groups, w/notifications and release notes"
+export DISTRIBUTION_GROUPS=x-test-group-1,x-test-group-2
+export NOTIFY_TESTERS=true
+export RELEASE_NOTES="Wow! Another test at $(date)!"
+bitrise run test
+
